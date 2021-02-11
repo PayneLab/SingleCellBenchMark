50e7af255a0bf21320f4e4ce8217a9af235ab540
hboekweg <hannahboekweg@gmail.com>
2021-02-11 13:19:17 -0700

    update 2ng parsers and figure

--- data_loader.py
+++ data_loader.py
@@ -13,10 +13,13 @@ def make_decoy_col_msfragger(row):
     else:
         return False
 
-
+def make_decoy_col_maxquant(row):
+    if row["Proteins"].startswith("REV"):
+        return True
+    else:
+        return False
 
 #load data
-
 def clean_msgfplus(file_name):
     msgfplus_files = {}
     #Single cell
@@ -31,8 +34,8 @@ def clean_msgfplus(file_name):
     msgfplus_files["50ng_4"] = "data/msgfplus/RC1051_Library_DDA_QC_HeLa_50ng_12-28-2020_Rep4.gz"
     msgfplus_files["50ng_5"] = "data/msgfplus/RC1051_Library_DDA_QC_HeLa_50ng_12-28-2020_Rep5.gz"
     #2 ng
-    msgfplus_files[".2ng"] = "data/msgfplus/Ex_Auto_DrM3_30umT4_2ngQC_60m_half.tsv.gz"
-    msgfplus_files["2ng"] = "data/msgfplus/Ex_Auto_DrM3_30umT4_02ngQC_60m_half.tsv.gz"
+    msgfplus_files["2ng"] = "data/msgfplus/Ex_Auto_DrM3_30umT4_2ngQC_60m_half.tsv.gz"
+    msgfplus_files[".2ng"] = "data/msgfplus/Ex_Auto_DrM3_30umT4_02ngQC_60m_half.tsv.gz"
 
 
     file_path = msgfplus_files.get(file_name)
@@ -89,58 +92,37 @@ def clean_msfragger(file_name):
 
     return df
 
+def clean_metamorph(file_name):
+    mm_files = {}
 
-def parse_meta(file_name, cutoff):
-    meta_files = {}
-    #Single cell
-    meta_files["singleCell_1"] = "data/MetaMorpheus/RC1051_DDA_SingleCell_HeLa_1-1-2021_Rep1_Peptides.psmtsv.gz"
-    meta_files["singleCell_2"] = "data/MetaMorpheus/RC1051_DDA_SingleCell_HeLa_1-1-2021_Rep2_Peptides.psmtsv.gz"
-    meta_files["singleCell_3"] = "data/MetaMorpheus/RC1051_DDA_SingleCell_HeLa_1-1-2021_Rep3_Peptides.psmtsv.gz"
-    meta_files["singleCell_4"] = "data/MetaMorpheus/RC1051_DDA_SingleCell_HeLa_1-14-2021_Rep1_Peptides.psmtsv.gz"
-    #50 ng
-    meta_files["50ng_1"] = "data/MetaMorpheus/RC1051_Library_DDA_QC_HeLa_50ng_12-28-2020_Rep1_Peptides.psmtsv.gz"
-    meta_files["50ng_2"] = "data/MetaMorpheus/RC1051_Library_DDA_QC_HeLa_50ng_12-28-2020_Rep2_Peptides.psmtsv.gz"
-    meta_files["50ng_3"] = "data/MetaMorpheus/RC1051_Library_DDA_QC_HeLa_50ng_12-28-2020_Rep3_Peptides.psmtsv.gz"
-    meta_files["50ng_4"] = "data/MetaMorpheus/RC1051_Library_DDA_QC_HeLa_50ng_12-28-2020_Rep4_Peptides.psmtsv.gz"
-    meta_files["50ng_5"] = "data/MetaMorpheus/RC1051_Library_DDA_QC_HeLa_50ng_12-28-2020_Rep5_Peptides.psmtsv.gz"
+    mm_files[".2ng"] = 'data/MetaMorpheus/Ex_Auto_DrM3_30umT4_02ngQC_60m_half_PSMs.psmtsv.gz'
+    mm_files["2ng"] = 'data/MetaMorpheus/Ex_Auto_DrM3_30umT4_2ngQC_60m_half_PSMs.psmtsv.gz'
 
-    #2ng run
-    meta_files[".2ng"] = ""
+    input_file = mm_files.get(file_name)
+    data = pd.read_csv(input_file, sep = '\t')
 
-    data = pd.read_csv(meta_files.get(file_name), sep="\t")
+    data = data.replace({"Decoy": {'Y': True, 'N': False}})
+    #uniform naming
+    data_new = data.rename({"Decoy": "decoy", "Scan Number": "scan", "Base Sequence": "peptide"}, axis=1)
+    data_new = data_new.filter((['decoy', 'scan', 'peptide','PEP_QValue' ]))
 
-    data.insert(len(data.columns), 'Decoy_Id', 1)
+    return data_new
 
-    decoy_count = 0;
-    for index, row in data['Cumulative Decoy'].iteritems():
-        decoy_num = int(data['Cumulative Decoy'][index])
-        if(decoy_num > decoy_count):
-            decoy_count +=1
-            data.iloc[index,len(data.columns)-1] = 'NaN'
+def clean_maxquant(file_name):
+    combined_df = pd.read_csv("data/maxquant/msmsScans.txt.gz", sep="\t")
 
+    #get the 2 ng file
+    maxq_files = {}
+    maxq_files["2ng"] = "Ex_Auto_DrM3_30umT4_2ngQC_60m_half"
+    maxq_files[".2ng"] = "Ex_Auto_DrM3_30umT4_02ngQC_60m_half"
+    file_path = maxq_files.get(file_name)
+    df = combined_df[combined_df["Raw file"]==file_path]
 
-    decoys_dropped_df = data[data.Decoy_Id != "NaN"]
+    df["decoy"] = df.apply(lambda row: make_decoy_col_maxquant(row), axis=1)
+    df = df.rename({"Scan number": "scan", "Sequence": "peptide"}, axis=1)
+    df = df.filter(['decoy', 'scan', 'peptide', 'PEP'])
 
-    no_duplicates_df = decoys_dropped_df.drop_duplicates(subset=['Scan Number'])
-    only_unique_df = no_duplicates_df.drop_duplicates(subset=['Base Sequence'])
-    cut_off_df = only_unique_df[only_unique_df.PEP_QValue <= 0.01]
-
-    return cut_off_df
-
-
-
-
-
-
-def print_stats(df):
-    #Find how many unique peptides there are
-    unique_pep = df['new_peptide'].nunique()
-    number_uniq_pep = df['new_peptide'].nunique()
-
-    #fine how many unique spectra there are
-    number_spectra = df["SpecID"].nunique()
-
-    print("Number of unique peptides:", number_uniq_pep,"   " "Number of unique spectra:", number_spectra)
+    return df
 
 
 #set cutoff
