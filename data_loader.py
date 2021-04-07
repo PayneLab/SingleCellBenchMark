0f696dc0e390759371b36e159545d2dae3fd55d9
Daisha Van Der Watt <daishavanderwatt@Daishas-MacBook-Pro.local>
2021-04-07 15:51:31 -0600

    added method to the dataloader to handle the pin files from MokaPot

--- data_loader.py
+++ data_loader.py
@@ -232,3 +232,29 @@ def clean_spectromine(file_name):
         # df = df.filter(['decoy', 'scan', 'peptide', 'probability'])
 
         return df
+    
+def get_pin_file(file_name):
+    mm_pin_files = {}
+
+    #2ng
+    mm_pin_files["2ng_rep1"] = "Ex_Auto_J3_30umTB_2ngQC_60m_1-calib_PSMsFormattedForPercolator.tab.gz"
+    mm_pin_files["2ng_rep2"] = "Ex_Auto_J3_30umTB_2ngQC_60m_2-calib_PSMsFormattedForPercolator.tab.gz"
+    mm_pin_files["2ng_rep3"] = "Ex_Auto_K13_30umTA_2ngQC_60m_1-calib_PSMsFormattedForPercolator.tab.gz"
+    mm_pin_files["2ng_rep4"] = "Ex_Auto_K13_30umTA_2ngQC_60m_2-calib_PSMsFormattedForPercolator.tab.gz"
+    mm_pin_files["2ng_rep5"] = "Ex_Auto_W17_30umTB_2ngQC_60m_1-calib_PSMsFormattedForPercolator.tab.gz"
+    mm_pin_files["2ng_rep6"] = "Ex_Auto_W17_30umTB_2ngQC_60m_2-calib_PSMsFormattedForPercolator.tab.gz"
+
+    #0.2 ng
+    mm_pin_files["0.2ng_rep1"] = "Ex_Auto_J3_30umTB_02ngQC_60m_1-calib_PSMsFormattedForPercolator.tab.gz"
+    mm_pin_files["0.2ng_rep2"] = "Ex_Auto_J3_30umTB_02ngQC_60m_2-calib_PSMsFormattedForPercolator.tab.gz"
+    mm_pin_files["0.2ng_rep3"] = "Ex_Auto_K13_30umTA_02ngQC_60m_1-calib_PSMsFormattedForPercolator.tab.gz"
+    mm_pin_files["0.2ng_rep4"] = "Ex_Auto_K13_30umTA_02ngQC_60m_2-calib_PSMsFormattedForPercolator.tab.gz"
+    mm_pin_files["0.2ng_rep5"] = "Ex_Auto_W17_30umTA_02ngQC_60m_3-calib_PSMsFormattedForPercolator.tab.gz"
+    mm_pin_files["0.2ng_rep6"] = "Ex_Auto_W17_30umTA_02ngQC_60m_4-calib_PSMsFormattedForPercolator.tab.gz"
+    
+    path_to_data_loader = os.path.abspath(os.path.dirname(__file__)) # This gets the absolute path to the location of the data_loader.py file
+    complete_path_to_data = os.path.join(path_to_data_loader, mm_pin_files.get(file_name)) # We then append the relative path to the data files
+
+    df = pd.read_csv(complete_path_to_data, sep = '\t')
+
+    return df
